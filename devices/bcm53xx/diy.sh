fd50d41ba7be760aada83d99915984f467cdc09f
lyhname <62424090+lyhname@users.noreply.github.com>
2022-07-23 15:24:52 +0800

    Update diy.sh

--- devices/bcm53xx/diy.sh
+++ devices/bcm53xx/diy.sh
@@ -5,5 +5,5 @@ shopt -s extglob
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 #bash $SHELL_FOLDER/../common/kernel_5.15.sh
 
-sed -i 's/DEFAULT_PACKAGES +=/DEFAULT_PACKAGES += autocore-arm luci-app-cpufreq luci-app-turboacc/' target/linux/mediatek/Makefile
+sed -i 's/DEFAULT_PACKAGES +=/DEFAULT_PACKAGES += autocore-arm luci-app-cpufreq luci-app-turboaccluci-app-adguardhome luci-app-aria2 luci-app-cifs-mount luci-app-ramfree luci-app-samba4 luci-app-smartdns luci-app-turboacc luci-app-ddns uci-app-aliyundrive-webdav luci-app-filebrowser luci-app-hd-idle luci-app-serverchan luci-app-vlmcsd luci-app-wireguard/' target/linux/mediatek/Makefile
 
